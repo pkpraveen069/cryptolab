8074e46d9646d03424e7066dcf7ba03a051ff12e
pkpraveen069 <115776146+pkpraveen069@users.noreply.github.com>
2024-04-20 20:50:09 +0530

    PlayfairCipher.java

--- PlayfairCipher.java
+++ PlayfairCipher.java
@@ -1,28 +1,15 @@
 import java.util.*;
 
 public class Main {
-    //info banner
-    public static void printbanner() {
-        System.out.println("'"
-        + "*******************************************************\n" +
-"     IT1681 - Cryptography and  Networks Security Laboratory\n" +
-"\n" +
-"     Roll Number : 21UIT056\n" +
-"     Name        :M.Praveen Kumar\n" +
-"     Ex. No.         : 02\n" +
-"     Ex. Name    : Implementation of Playfair Cipher \n" +
-"     Date        : 7.02.2023\n" +
-"*******************************************************\n" +
-"\n" +
-"     ------- ------- --------\n");
-    }
-    //function for Pre process the Given Text
+   
+   
+    
     public static ArrayList<String> preProcessText(String msg) {
-        //Variable Declaration
+       
         ArrayList<String>preprotext = new ArrayList<String>();
         String temp="";
         char prev,cor;int i=1;
-        //while to itrate to the length of message and do a pre processing
+        
         while( i < msg.length()) {
             temp+=prev=msg.charAt(i-1);
             cor=msg.charAt(i);
@@ -47,13 +34,13 @@ public class Main {
         System.out.println("preprocessed text: "+Arrays.toString(preprotext.toArray()));
         return preprotext;
     }
-    //function to gentrate the key matrix
+
     public static String[] genKey(String Ori_key) {
-        //variable declaration
+       
         String keymat[]= new String[5];
         String key="";
         int j=0;
-        //loop to itrate 0 to 91 ([0 : length(orignal_key)] and [65 : 90]) to add the alphabets with key to make key matrix
+       
         for (int i = 0; i <91 ; i++) {    
             if(i<Ori_key.length() && key.indexOf(Ori_key.charAt(i))==-1 ) {
                 if((Ori_key.charAt(i)=='I' ||  Ori_key.charAt(i)=='J') && (key.indexOf('I')>-1 || key.indexOf('J')>-1)) {
@@ -75,7 +62,7 @@ public class Main {
             }
            
         }
-        //split the entier string into String arrays because the key in Single string
+        
         String temp="";
         for (int i = 0; i < key.length(); i++) {
                     temp+=key.charAt(i);
@@ -89,12 +76,12 @@ public class Main {
              
        
     }
-    //function to encrypt and decrypt the given text based on the key
+   
     public static String operation(String msg , String key,String mode)
     {
-         //pre process the text
+     
          ArrayList<String>pptext = preProcessText(msg.toUpperCase());
-         //Gentrate the key matrix
+         
          String keymat[]=genKey(key.toUpperCase()),text="";
          
         String coloumntext="";int index1,index2;
@@ -102,14 +89,14 @@ public class Main {
         texti:for (int i = 0; i <pptext.size(); i++) {
            
             char char1=pptext.get(i).charAt(0),char2=pptext.get(i).charAt(1);
-            //change the i or j based on the what's in key matrx
+           
                 char1= (char1=='J' || char1=='I')?keymat[i%5].indexOf('J')>-1?'J':keymat[i%5].indexOf('I')>-1?'I':char1:char1;
                 char2= (char2=='J' || char2=='I')?keymat[i%5].indexOf('J')>-1?'J':keymat[i%5].indexOf('I')>-1?'I':char2:char2;
        
-            //j for row
+            
              for (int j = 0; j < 5; j++) {
              coloumntext="";
-            //For Rule 1
+
              index1=keymat[j].indexOf(char1); index2= keymat[j].indexOf(char2);
             if(index1>-1 && index2>-1 ){
                 if(mode.equalsIgnoreCase("encrypt")){
@@ -120,7 +107,7 @@ public class Main {
                 continue texti;
                 }
             }
-            //For Rule 2
+           
             for(int k=0; k<5; k++)coloumntext+=keymat[k].charAt(j);
             index1=coloumntext.indexOf(char1); index2= coloumntext.indexOf(char2);
             if(index1>-1 &&index2>-1 ){
@@ -136,7 +123,7 @@ public class Main {
            
            
         }
-        //For Rule 3
+      
             for (int k = 0; k < 5; k++) {
                 index1=keymat[k].indexOf(char1);
                 if(index1>-1) {
@@ -154,11 +141,10 @@ public class Main {
 
         return mode.equalsIgnoreCase("encrypt")?"CipherText: "+text:"PlainText: "+(text.toLowerCase());
     }
-    //main method to print the banner and choices
+   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        printbanner();
-        String options="Play Fair Cipher\n----------------\n1.Encryption\n2.Decryption\n3.Exit\n",text,key;        
+        String options="1.Encryption\n2.Decryption\n3.Exit\n",text,key;        
         int choice;
          do {
             System.out.println(options+"Enter your Choice: ");
